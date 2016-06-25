9a6d37e999194e0f3eddda3b93bbd8940324978d
ipjohnson <ipjohnson123@yahoo.com>
2016-06-25 10:00:49 -0600

    Adding Grace.AspNet.MVC

---
+++ src/Grace.AspNet.MVC/GraceControllerActivator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+
+namespace Grace.AspNet.MVC
+{
+    public class GraceControllerActivator : IControllerActivator
+    {
+        public object Create(ControllerContext context)
+        {
+            return context.HttpContext.RequestServices.GetService(context.ActionDescriptor.ControllerTypeInfo.AsType());
+        }
+
+        public void Release(ControllerContext context, object controller)
+        {
+
+        }
+    }
+}
