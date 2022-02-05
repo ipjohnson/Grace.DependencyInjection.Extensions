3b5718622e423a2b26c82cd9233f6a88c187342a
Ian Johnson <ipjohnson123@yahoo.com>
2022-02-05 10:38:23 -0500

    making GraceServiceProviderFactory public for issue 32

--- src/Grace.AspNetCore.Hosting/GraceServiceProviderExtensions.cs
+++ src/Grace.AspNetCore.Hosting/GraceServiceProviderExtensions.cs
@@ -39,42 +39,5 @@ namespace Grace.AspNetCore.Hosting
             return builder.ConfigureServices(c => c.AddSingleton<IServiceProviderFactory<IInjectionScope>>(new GraceServiceProviderFactory(configuration)));
         }
 
-        private class GraceServiceProviderFactory : IServiceProviderFactory<IInjectionScope>
-        {
-            private readonly IInjectionScopeConfiguration _configuration;
-
-            /// <summary>
-            /// Default constructor
-            /// </summary>
-            /// <param name="configuration"></param>
-            public GraceServiceProviderFactory(IInjectionScopeConfiguration configuration)
-            {
-                _configuration = configuration ?? new InjectionScopeConfiguration();
-            }
-
-            /// <summary>
-            /// Creates a container builder from an <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" />.
-            /// </summary>
-            /// <param name="services">The collection of services</param>
-            /// <returns>A container builder that can be used to create an <see cref="T:System.IServiceProvider" />.</returns>
-            public IInjectionScope CreateBuilder(IServiceCollection services)
-            {
-                var container = new DependencyInjectionContainer(_configuration);
-
-                container.Populate(services);
-
-                return container;
-            }
-
-            /// <summary>
-            /// Creates an <see cref="T:System.IServiceProvider" /> from the container builder.
-            /// </summary>
-            /// <param name="containerBuilder">The container builder</param>
-            /// <returns>An <see cref="T:System.IServiceProvider" /></returns>
-            public IServiceProvider CreateServiceProvider(IInjectionScope containerBuilder)
-            {
-                return containerBuilder.Locate<IServiceProvider>();
-            }
-        }
     }
 }
---
+++ src/Grace.Extensions.Hosting/GraceServiceProviderFactory.cs
@@ -0,0 +1,47 @@
+﻿using Grace.DependencyInjection;
+using Grace.DependencyInjection.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Grace.Extensions.Hosting
+{
+    public class GraceServiceProviderFactory : IServiceProviderFactory<IInjectionScope>
+    {
+        private readonly IInjectionScopeConfiguration _configuration;
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        /// <param name="configuration"></param>
+        public GraceServiceProviderFactory(IInjectionScopeConfiguration configuration)
+        {
+            _configuration = configuration ?? new InjectionScopeConfiguration();
+        }
+
+        /// <summary>
+        /// Creates a container builder from an <see cref="T:Microsoft.Extensions.DependencyInjection.IServiceCollection" />.
+        /// </summary>
+        /// <param name="services">The collection of services</param>
+        /// <returns>A container builder that can be used to create an <see cref="T:System.IServiceProvider" />.</returns>
+        public IInjectionScope CreateBuilder(IServiceCollection services)
+        {
+            var container = new DependencyInjectionContainer(_configuration);
+
+            container.Populate(services);
+
+            return container;
+        }
+
+        /// <summary>
+        /// Creates an <see cref="T:System.IServiceProvider" /> from the container builder.
+        /// </summary>
+        /// <param name="containerBuilder">The container builder</param>
+        /// <returns>An <see cref="T:System.IServiceProvider" /></returns>
+        public IServiceProvider CreateServiceProvider(IInjectionScope containerBuilder)
+        {
+            return containerBuilder.Locate<IServiceProvider>();
+        }
+    }
+}
